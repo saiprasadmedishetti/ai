6c09ac35051adf5e8b877f68bffa0979e89fb192
Saiprasad Medishetti <saiprasadmedishetti@Saiprasads-MacBook-Air.local>
2024-11-15 19:32:32 +0530

    feature: added markdown

--- src/components/ai.tsx
+++ src/components/ai.tsx
@@ -3,6 +3,7 @@ import { askAI } from "../utils/ai";
 import CodeBlock from "./CodeBlock";
 import AiIcon from "../assets/ai.png";
 import Search from "./Search";
+import ReactMarkdown from "react-markdown";
 
 type Messages = {
   type: "in" | "out";
@@ -26,6 +27,7 @@ export default function Ai() {
   return (
     <div className="flex flex-col px-2 py-8 h-full">
       <div className="flex-1 overflow-auto">
+        <div className="pr-3">
         <h1 className="text-lg font-semibold text-center">Welcome to Ai</h1>
 
         {answers.map((answer, idx) => (
@@ -36,25 +38,29 @@ export default function Ai() {
             key={idx}
           >
             {answer.type === "out" ? (
-              <span className="py-3 px-4 rounded-full bg-[#2f2f2f]">
+              <span className="py-2 px-4 rounded-full bg-[#2f2f2f]">
                 {answer.message}
               </span>
             ) : (
-              <div className="flex items-center gap-2">
+              <div className="flex items-start gap-2 max-w-100 items-start">
                 <img
                   className="bg-[#2f2f2f] p-2 h-12 rounded-full"
                   src={AiIcon}
                   alt="ai"
                 />
-                {answer.message.slice(0, 3) === "```" ? (
+                {/* {answer.message.slice(0, 3) === "```" ? (
                   <CodeBlock text={answer.message} />
                 ) : (
                   answer.message
-                )}
+                )} */}
+                <div className="response-container flex flex-col">
+                  <ReactMarkdown>{answer.message}</ReactMarkdown>
+                </div>
               </div>
             )}
           </div>
         ))}
+        </div>
       </div>
       <div className="relative shrink-0">
         <form onSubmit={onSubmit}>
